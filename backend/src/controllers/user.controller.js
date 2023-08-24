ebbc034b62fb7a4c282ccb316a3aa7d0fba1bbfa
Soeun Lee <thdms1005@gmail.com>
2023-08-24 16:36:25 +0200

    signup confirmPW in user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -19,6 +19,7 @@ const signupUser = async (req, res) => {
     const user = await User.signup(
       email,
       password,
+      confirmPassword,
       username,
       firstname,
       lastname
