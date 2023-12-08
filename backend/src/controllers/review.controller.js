57d1844459c2f622889feb649051a8ccc39a0738
Soeun Lee <thdms1005@gmail.com>
2023-12-08 10:34:56 +0100

    :coffin: Refactor(controller) : remove useless codes

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -48,63 +48,6 @@ const createReview = async (req, res) => {
   }
 };
 
-// check if review already exist
-const checkReviewStatus = async (req, res) => {
-  const { mediaId } = req.body;
-  const userId = req.user._id;
-
-  // add data to db
-  try {
-    const review = await Review.findOne({
-      mediaId,
-      userId,
-    });
-
-    if (review) {
-      res.status(200).json({ hasReview: true });
-    } else {
-      res.status(200).json({ hasReview: false });
-    }
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
-
-// delete a review
-const deleteReview = async (req, res) => {
-  // console.log(typeof req.params.id);
-
-  try {
-    const { id: mediaId } = req.params;
-    const result = await Review.deleteOne({ mediaId: mediaId });
-
-    if (result.deletedCount === 0) {
-      return res.status(404).json({ error: 'No such review' });
-    }
-
-    res.status(200).json({ message: 'Review deleted successfully' });
-  } catch (error) {
-    res.status(404).json({ error: 'No such review' });
-  }
-};
-
-// edit review
-const editReview = async (req, res) => {
-  try {
-    // movie id
-    const { id } = req.params;
-    // console.log(id);
-    const result = await Review.findOneAndUpdate({ mediaId: id }, req.body, {
-      new: true,
-    });
-    res.status(200).json(result);
-    console.log('Update succeeded');
-  } catch (error) {
-    res.status(404).json({ error: 'Review update failed' });
-    console.log(error);
-  }
-};
-
 // load reviews and the movie info that matches the review by id
 // fetch movie information and credit here when it's called
 const loadReviews = async (req, res) => {
@@ -158,10 +101,64 @@ const loadReviews = async (req, res) => {
   }
 };
 
+// check if review already exist
+const checkReviewStatus = async (req, res) => {
+  const { mediaId } = req.body;
+  const userId = req.user._id;
+
+  try {
+    const review = await Review.findOne({
+      mediaId,
+      userId,
+    });
+
+    if (review) {
+      res.status(200).json({ hasReview: true });
+    } else {
+      res.status(200).json({ hasReview: false });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// delete a review
+const deleteReview = async (req, res) => {
+  try {
+    const { id: mediaId } = req.params;
+    const result = await Review.deleteOne({ mediaId: mediaId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: 'No such review' });
+    }
+
+    res.status(200).json({ message: 'Review deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ error: 'No such review' });
+  }
+};
+
+// edit review
+const editReview = async (req, res) => {
+  try {
+    // movie id
+    const { id } = req.params;
+    // console.log(id);
+    const result = await Review.findOneAndUpdate({ mediaId: id }, req.body, {
+      new: true,
+    });
+    res.status(200).json(result);
+    console.log('Update succeeded');
+  } catch (error) {
+    res.status(404).json({ error: 'Review update failed' });
+    console.log(error);
+  }
+};
+
 export default {
   createReview,
+  loadReviews,
   checkReviewStatus,
   deleteReview,
   editReview,
-  loadReviews,
 };
