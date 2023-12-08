f647c75c7628cd6e88e0457bb692fc21d19f4feb
Soeun Lee <thdms1005@gmail.com>
2023-12-08 11:41:20 +0100

    :art: Refactor(apifetch) : manage data length in the back

--- frontend/src/api/apiFetchService.jsx
+++ frontend/src/api/apiFetchService.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ApiFetchService = (endpoint, numberOfData) => {
+const ApiFetchService = (endpoint) => {
   const [data, setData] = useState(null);
   const [loading, setloading] = useState(true);
   const [error, seterror] = useState('');
@@ -9,10 +9,10 @@ const ApiFetchService = (endpoint, numberOfData) => {
   useEffect(() => {
     axios(endpoint).then((res) => {
       seterror(res.error);
-      setData(res.data.slice(0, numberOfData)); // 첫 N개 요소만 추출하여 저장
+      setData(res.data); // 첫 N개 요소만 추출하여 저장
       setloading(false);
     });
-  }, [endpoint, numberOfData]);
+  }, [endpoint]);
 
   return { data, loading, error };
 };
