1a82d772aea70e1075488d4db7c5df3b7618a3ba
Soeun Lee <thdms1005@gmail.com>
2023-12-11 15:45:58 +0100

    :lipstick: Design(carousel) : make carousel responsive

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -6,23 +6,46 @@ const Carousel = ({ children, title }) => {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     autoplay: true,
-    speed: 3000,
-    autoplaySpeed: 3000,
+    speed: 1500,
+    autoplaySpeed: 4000,
     cssEase: 'linear',
     swipeToSlide: true,
-    afterChange: function (index) {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    },
+    arrows: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div>
-      <h2>{title}</h2>
+      <h2 className="section-title">{title}</h2>
       <Slider {...settings}>{children}</Slider>
     </div>
   );
