dc008ea9440af73699613fa36ee76e4903a5b0b1
Soeun Lee <thdms1005@gmail.com>
2023-12-13 17:15:52 +0100

    :adhesive_bandage: Fix(date) : format date with dayjs for filmoCard

--- frontend/src/components/FilmoCard.jsx
+++ frontend/src/components/FilmoCard.jsx
@@ -1,8 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import fallback from '../assets/fallback_img.png';
+import dayjs from 'dayjs';
 
 const FilmoCard = ({ poster, title, date, path, character }) => {
   const posterSrc = poster.includes('null') ? fallback : poster;
+  const formattedDate = dayjs(date).format('DD/MM/YYYY');
 
   return (
     <NavLink to={path} className="filmocard__container">
@@ -12,7 +14,7 @@ const FilmoCard = ({ poster, title, date, path, character }) => {
       <div className="filmocard-info-wrap">
         <div className="filmocard-info-wrap__title">
           <span>{title} </span>
-          <span>({date})</span>
+          <span>({formattedDate})</span>
         </div>
         <div className="filmocard-info-wrap__credit">
           <span>Credit: </span>
