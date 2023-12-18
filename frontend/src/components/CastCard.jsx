9a67f412e2196c81ea92367c8b846ab8344d2e52
Soeun Lee <thdms1005@gmail.com>
2023-12-18 15:40:47 +0100

    :adhesive_bandage: Fix(cast) : fix a character name display problem

--- frontend/src/components/CastCard.jsx
+++ frontend/src/components/CastCard.jsx
@@ -2,34 +2,20 @@ import { NavLink } from 'react-router-dom';
 import FallbackPoster from './FallbackPoster';
 
 const CastCard = ({ castInfo }) => {
-  // cast info =>
+  const { id, key, name, character, profile_path } = castInfo;
 
-  //   cast_id: 1;
-  //   character: 'Carol Danvers / Captain Marvel';
-  //   credit_id: '5d08fd3bc3a3687c751ef54c';
-  //   id: 60073;
-  //   name: 'Brie Larson';
-  //   order: 0;
-  //   original_name: 'Brie Larson';
-  //   popularity: 121.943;
-  //   profile_path: '/80DH2zWgZiXHehH7TLe6HKDldyl.jpg';
-
-  const castPhoto = `https://image.tmdb.org/t/p/w185/${castInfo.profile_path}`;
+  const castPhoto = `https://image.tmdb.org/t/p/w185/${profile_path}`;
   const posterSrc = FallbackPoster(castPhoto);
 
   return (
     <div className="cast-card">
-      <NavLink
-        to={`/cast/${castInfo.id}`}
-        className="cast-card-wrap"
-        key={castInfo.key}
-      >
+      <NavLink to={`/cast/${id}`} className="cast-card-wrap" key={key}>
         <div className="img__container">
-          <img src={posterSrc} alt={castInfo.name} className="cast-card-img" />
+          <img src={posterSrc} alt={name} className="cast-card-img" />
         </div>
         <div className="name__container">
-          <p>{castInfo.name}</p>
-          <p>/{castInfo.character}</p>
+          <p>{name}</p>
+          {character !== '' && <p>/{character}</p>}
         </div>
       </NavLink>
     </div>
